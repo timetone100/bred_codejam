ffccb6613569b6252b78bba21e43dc84ede516ae
timetone100 <49171767+timetone100@users.noreply.github.com>
2019-07-29 10:49:17 -0400

    Update right_justify.py

--- right_justify.py
+++ right_justify.py
@@ -4,3 +4,14 @@ def right_justify(s):
     print(' ' * (70-len(s)) + s)
 
 right_justify(dorian_name)
+
+
+# Ive been playing around with adding a subtracting components to the line in question (4)
+
+# The space in between the quotes is significant, but doesn't break the code
+
+# with the quotes right next to eachoter the name prints all the way to the left
+
+# with the space in between it follows the 70 rule and ends on 70.
+
+# i don't really understand why..
